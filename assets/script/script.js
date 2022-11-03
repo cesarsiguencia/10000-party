9724678e149d19fcd91e85f564bd849860bd200e
cesarsiguencia <cesar.siguencia@gmail.com>
2022-11-02 21:20:58 -0400

    final layout of intro html pg complete

---
+++ assets/script/script.js
@@ -0,0 +1,11 @@
+var welcomeBox = document.querySelector(".welcome-box")
+var welcomeMenu = document.querySelector(".hero-menu")
+
+setTimeout(() => {
+    welcomeBox.classList.add("hero-menu-animation")
+}, 1000)
+
+setTimeout(() => {
+    welcomeMenu.classList.add("hero-menu-animation")
+}, 4000)
+
