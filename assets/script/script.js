1d4c37d298033bdf88844278a0caa5db5ee660da
cesarsiguencia <cesar.siguencia@gmail.com>
2022-11-04 11:30:38 -0400

    background image solved

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,11 +1,44 @@
-var welcomeBox = document.querySelector(".welcome-box")
+var welcomeBox = document.querySelector(".carousel")
+var createItem = ''
 var welcomeMenu = document.querySelector(".hero-menu")
 
-setTimeout(() => {
-    welcomeBox.classList.add("welcome-box-animation")
-}, 1000)
+// setTimeout(() => {
+//     welcomeBox.classList.add("welcome-box-animation")
+// }, 1000)
 
 setTimeout(() => {
     welcomeMenu.classList.add("hero-menu-animation")
-}, 4000)
+}, 8000)
 
+
+
+var i = 0
+var words = ["You're Invited!", "On my 10,000th day of life", "Saturday, October 21st, 2023"]
+var time = 4000
+
+function changeWords(){
+
+    if (createItem){
+        createItem.remove()
+    }
+
+    createItem = document.createElement("div");
+    createItem.className = 'carousel-item';
+    createItem.className = 'welcome-box-animation'
+    createItem.innerHTML = "<h1>" + words[i] + "</h1>"
+
+
+    welcomeBox.appendChild(createItem)
+    carouselItem = createItem
+
+    if(i < words.length-1){
+        i++
+    } else {
+        return
+    }
+
+    setTimeout("changeWords()", time)
+    
+}
+
+window.onload=changeWords
