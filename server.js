4aaabcc90d12b1f19b6f216a7d7afde5ee59b4f5
cesarsiguencia <cesar.siguencia@gmail.com>
2023-03-30 20:16:33 -0400

    basic server constructed, fully functional

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+
+const sequelize = require ('sequelize')
+
+const app = express()
+
+const PORT = process.env.PORT || 3300
+
+//middleware for json and it's key/values
+app.use(express.json());
+app.use(express.urlencoded({extended: true}))
+
+app.get('/', (req, res) => {
+    res.json('working file')
+})
+
+// sequelize.sync({ force: true }).then(() => {
+//     app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
+// })
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
