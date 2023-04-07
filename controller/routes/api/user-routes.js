8bd0e604a5341635d43665acda1318c7da236e42
cesarsiguencia <cesar.siguencia@gmail.com>
2023-04-07 09:41:53 -0400

    added for single user api

--- controller/routes/api/user-routes.js
+++ controller/routes/api/user-routes.js
@@ -12,4 +12,21 @@ router.get('/', (req,res) => {
         })
 });
 
+router.get('/:id', (req,res) => {
+    User.findOne({
+        attributes: { exclude: ['password']},
+        where: {
+            id: req.params.id
+        }
+    }).then(userFromDb => {
+        if(!userFromDb){
+            res.status(404).json({ message: 'User does not exist'})
+            return;
+        }
+        res.json(userFromDb)
+    }).catch(err => {
+        res.status(500).json(err)
+    })
+})
+
 module.exports = router;
