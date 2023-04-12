59e5a41485843e371be74b6f27890e56b370dfb2
cesarsiguencia <cesar.siguencia@gmail.com>
2023-04-12 08:58:42 -0400

    added parallax effect to venue imgs in home page

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,19 +1,20 @@
 var welcomeBox = document.querySelector(".intro-box")
 var carouselBox = document.querySelector("#carousel-words")
-var createItem = ''
+var wordBox = ''
 var i = 0
-var words = ["You're Invited!", "On my 10,000th day of life", "Saturday, October 21st, 2023"]
-var time = 4000
+
 
 function changeWords(){
-    if (createItem){
-        createItem.remove()
-    }
-    createItem = document.createElement("div");
-    createItem.className = 'intro-box-words'
-    createItem.innerHTML = "<p class='welcome-title'>" + words[i] + "</p>"
+    var words = ["You're Invited!", "On my 10,000th day of life", "Saturday, October 21st, 2023"]
 
-    carouselBox.appendChild(createItem)
+    if(wordBox){
+        wordBox.remove()
+    }
+    wordBox = document.createElement("div");
+    wordBox.className = 'intro-box-words'
+    wordBox.innerHTML = "<p class='welcome-title'>" + words[i] + "</p>"
+
+    carouselBox.appendChild(wordBox)
 
     if(i < words.length-1){
         i++
@@ -21,7 +22,7 @@ function changeWords(){
         return
     }
 
-    setTimeout("changeWords()", time)
+    setTimeout("changeWords()", 4000)
 }
 
 function insertHomeBtn(){
@@ -38,31 +39,55 @@ function appearButton(){
 }
 
 
-
 function introParallax(event){
-    // const h1 = document.getElementById("h1"),
     const hero = document.querySelector(".hero")
-    // button = document.getElementById("button");
-    
     const scrollPosition = event.target.scrollingElement.scrollTop;
+    
     console.log(scrollPosition)
     if (scrollPosition > 20) {
         hero.style.height = "100vh";
     } else {
         hero.style.height = "120vh";
-    }
-    
-    
+    } 
 } 
 
 function loadParallax(event){
+    const scrollPosition = event.target.scrollingElement.scrollTop;
     const hero = document.querySelector(".hero")
     hero.style.height = "120vh";
     event.target.scrollingElement.scrollTop = 0
 
 }
 
+function homeParallax(event){
+    const hero2 = document.querySelector("#hero-2")
+    const scrollPosition = event.target.scrollingElement.scrollTop;
+    if(scrollPosition > 350){
+        hero2.style.opacity = 0;
+        hero2.style.translate = "0 -50px";
+        hero2.style.scale = "0.9";
+    } else {
+        hero2.style.opacity = 1;
+        hero2.style.translate = 0;
+        hero2.style.scale = 1;
+    }
+}
 
+function venueImgsParallax(event){
+    const skylark1 = document.querySelector(".skylark-imgs")
+    const skylark2 = document.querySelector(".skylark-imgs-2")
+    const skylark3 = document.querySelector(".skylark-imgs-3")
+    const scrollPosition = event.target.scrollingElement.scrollTop;
+    if(scrollPosition > 2000){
+        skylark1.style.backgroundSize = "230%"
+        skylark2.style.backgroundSize = "200%"
+        skylark3.style.backgroundSize = "200%"
+    } else {
+        skylark1.style.backgroundSize = "210%"
+        skylark2.style.backgroundSize = "180%"
+        skylark3.style.backgroundSize = "180%"
+    }
+}
 
 
 
@@ -71,3 +96,5 @@ window.onload=loadParallax
 setTimeout("changeWords()", 1000)
 setTimeout("insertHomeBtn()", 1000)
 setTimeout("appearButton()", 12000)
+document.addEventListener("scroll", homeParallax)
+document.addEventListener("scroll", venueImgsParallax)
