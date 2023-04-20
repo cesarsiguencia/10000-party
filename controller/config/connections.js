7bab21d1450a00eefaf79840d8a894ad36b91a36
cesarsiguencia <cesar.siguencia@gmail.com>
2023-04-20 13:27:42 -0400

    second attempt at uploading to cyclic

--- controller/config/connections.js
+++ controller/config/connections.js
@@ -5,19 +5,21 @@ require('dotenv').config()
 let sequelize;
 
 if(process.env.CYCLIC_URL){
-    sequelize = new Sequelize(process.env.CYCLIC_URL, {
-        dialect:  'mysql',
-        protocol: 'mysql',
-        logging:  true //false
-    }) 
+    sequelize = new Sequelize(process.env.CYCLIC_DB, process.env.CYCLIC_APP_ID,process.env.CYCLIC_URL, CYCLIC_BUCKET_NAME, 
+    //     {
+    //     dialect:  'mysql',
+    //     protocol: 'mysql',
+    //     logging:  true 
+    // }
+    ) 
 } else {
     sequelize = new Sequelize(
-        process.env.CYCLIC_URL,
+        // process.env.CYCLIC_URL,
         process.env.mysql_database_name, 
         process.env.mysql_user, 
         process.env.mysql_password, 
         {
-            host: 'localhost',
+            // host: 'localhost',
             dialect: 'mysql',
             port: 3306
         }
